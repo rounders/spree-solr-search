deaf5f1e8afa4ee65933c04b92963457b909bde3
Francois Harbec <fharbec@gmail.com>
2011-11-01 14:46:14 -0700

    add ability to do taxon searches

--- lib/spree/search/solr.rb
+++ lib/spree/search/solr.rb
@@ -1,6 +1,27 @@
 module Spree::Search
   class Solr < defined?(Spree::Search::MultiDomain) ? Spree::Search::MultiDomain :  Spree::Search::Base
+    def retrieve_products
+      base_scope = get_base_scope
+      @products_scope = @product_group.apply_on(base_scope)
+      curr_page = manage_pagination ? 1 : page
+
+      @products = @products_scope.paginate({
+        :include  => [:images, :master],
+        :per_page => per_page,
+        :page     => curr_page
+        })
+    end
     protected
+    
+    def get_base_scope
+      base_scope = @cached_product_group ? @cached_product_group.products.active : Product.active
+      base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
+      base_scope = get_products_conditions_for(base_scope, keywords) unless (keywords.blank? && taxon.blank?)
+
+      base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
+      base_scope = base_scope.group_by_products_id if @product_group.product_scopes.size > 1
+      base_scope
+    end
 
     def get_products_conditions_for(base_scope, query)
       facets = {
@@ -12,7 +33,7 @@ module Spree::Search
       if order_by_price
         search_options.merge!(:order => (order_by_price == 'descend') ? "price desc" : "price asc")
       end
-      full_query = query + " AND is_active:(true)"
+      full_query = query.blank? ? "is_active:(true)" : query + " AND is_active:(true)"
       if taxon 
         taxons_query = taxon.self_and_descendants.map{|t| "taxon_ids:(#{t.id})"}.join(" OR ")
         full_query += " AND (#{taxons_query})"
