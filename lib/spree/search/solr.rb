4bde59e37f46476c472b56462cb244cce2530eec
Francois Harbec <fharbec@gmail.com>
2011-11-23 16:01:02 -0800

    remove unecessary scope

--- lib/spree/search/solr.rb
+++ lib/spree/search/solr.rb
@@ -15,7 +15,6 @@ module Spree::Search
     
     def get_base_scope
       base_scope = @cached_product_group ? @cached_product_group.products.active : Product.active
-      base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
       base_scope = get_products_conditions_for(base_scope, keywords) unless (keywords.blank? && taxon.blank?)
 
       base_scope = base_scope.on_hand unless Spree::Config[:show_zero_stock_products]
